046e652a2bcffc97fd519e526ce0036f9e1372c9
Fl0wyn <florian+github@feranet.fr>
2023-09-23 17:37:56 +0200

    up

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
+<script setup>
 </script>
 
 <template>
-  <RouterView />
-  <!--   <div class="flex items-center min-h-screen justify-center">
-    <div class="p-8 shadow flex flex-col items-center">
-      <img class="h-12" src="./assets/logo.svg" alt="Logo Vue 3" />
-      <RouterView />
-      <button class="bg-[#41b883] hover:bg-[#34495e] hover:text-white rounded-full px-3 py-1" v-if="$route.path === '/'">
-        <RouterLink to="/page2">Go to page 2</RouterLink>
-      </button>
-    </div>
-  </div> -->
+  <picture class="text-6xl">🚀</picture>
+  <h1 class="text-6xl mt-6 font-bold">Starter - Vue Tailwind</h1>
 </template>
--- src/stores/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const useAppStore = defineStore('app', {
-  state: () => ({
-    app_title: 'Starter - Vue Tailwind'
-  })
-})
